ab302141c2523e32a78c4d6b6843ce25892bf29f
Nikos Katsiantonis <n.katsiantonis@gmail.com>
2016-10-17 00:44:53 +0300

    Add files via upload

--- dna_store.py
+++ dna_store.py
@@ -1,4 +1,6 @@
 
+import re
+
 def huffman_code(huffman, x, code=''):
 	if ('left' in huffman) and (x in huffman['left']['chars']):		
 		code += '0'
@@ -8,6 +10,22 @@ def huffman_code(huffman, x, code=''):
 		return huffman_code(huffman['right'], x, code)
 	else:
 		return code
+
+
+def huffman_encode(source, huffman_table):
+ 	return ''.join(list(map(lambda c: huffman_table[c], source)))
+
+def huffman_decode(encoded, huffman_table_inv):
+	codes = '|'.join(huffman_table_inv.keys())
+	regex_codes = re.compile(codes)
+	decoded = ""
+	for code in regex_codes.finditer(encoded):
+		decoded += huffman_table_inv[code.group()]
+	return decoded
+
+
+
+
 
 source = "hello, world"
 
@@ -45,4 +63,22 @@ while (len(huffman_queue) > 1):
 huffman_tree = huffman_queue[0]
 #print(huffman_tree)
 
-print(huffman_code(huffman_tree, 'o'))
+#print(huffman_code(huffman_tree, 'o'))
+
+
+huffman_table = {}
+huffman_table_inv = {}
+for k in freq.keys():
+	code = huffman_code(huffman_tree, k)
+	huffman_table[k] = code
+	huffman_table_inv[code] = k
+
+print(huffman_table)
+print(huffman_table_inv)
+
+encoded = huffman_encode(source, huffman_table)
+print(encoded)
+
+
+decoded = huffman_decode(encoded, huffman_table_inv)
+print(decoded)
